21e4d326b7aa5dea1dd256c7380d6acdc9b75af8
root <root@localhost.localdomain>
2024-11-10 00:16:33 -0600

    update frontend v2 busqueda

--- src/views/BusquedaView.vue
+++ src/views/BusquedaView.vue
@@ -1,16 +1,25 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
+import ComponentBusquedaProducto from '@/components/Component-busqueda-producto.vue';
 
 </script>
 <template>
     <div class="Busqueda">
-        <h1>hello esto es para dar busquedas</h1>
+        <ComponentBusquedaProducto/>
         <Navbar/>
     </div>
   </template>
   
   <style>
-
+  .Busqueda{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: calc(100% - 0px);
+    height: 100vh;
+    overflow-y: auto; /* Permite el desplazamiento solo vertical */
+    padding: 1rem;
+  }
   @media (min-width: 1024px) {
     .about {
       min-height: 100vh;
