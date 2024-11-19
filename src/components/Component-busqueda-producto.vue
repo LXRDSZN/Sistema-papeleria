0d8b6dea0c14f97044ae8381720ad42e59922d88
AndresDevMaster <andresdevmaster@gmail.com>
2024-11-19 10:14:57 -0600

    Modificacion de ventana "Ventas"

--- src/components/Component-busqueda-producto.vue
+++ src/components/Component-busqueda-producto.vue
@@ -20,7 +20,7 @@ const datosDeCompra = [
             <label>Forma de Busqueda</label>
             <select>
               <option>Seleccione la forma</option>
-              <option>CD</option>
+              <option>Codigo de Barras</option>
               <option>Nombre</option>
             </select>
           </div>
